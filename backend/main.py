e0b47e1c59e9dd1711bda6af37dbd3befec46566
Sai Varshith <saivarshithjoy025@gmail.com>
2024-07-28 17:01:09 +0530

    backend commit

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ security = HTTPBasic()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://sql-assistant-indol.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
